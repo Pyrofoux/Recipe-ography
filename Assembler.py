9fd3963cf3c136cbbe1e5de17f2b227fccf73f54
Younès <rabiiyounes@outlook.com>
2021-06-11 11:46:50 +0200

    Webviewer: displaying country names

--- Assembler.py
+++ Assembler.py
@@ -8,8 +8,9 @@ from CultureNameGen import *
 # They could be stored in a settings.ini file instead
 
 settings = {}
-settings["mapgen_iterationCount"]       = 10
-settings["mapgen_startMapType"]         = 1
+settings["mapgen_iterationCount"]        = 10
+settings["mapgen_startMapType"]          = 1
+settings["culturespread_iterationCount"] = 100
 
 
 # Starting Assembling
@@ -18,18 +19,33 @@ output = {} #output json file
 output_file_name = "./webviewer/world_data.js"
 
 # Generates terrain
-terrain_map = np.array(generate_terrain_matrix(settings["mapgen_startMapType"], settings["mapgen_iterationCount"]))
+terrain_maps = np.array(generate_terrain_matrix(settings["mapgen_startMapType"], settings["mapgen_iterationCount"]))
 
-output["terrain_steps"] = terrain_map.shape[0] #convert terrain to JSON serializable format
-output["terrain_width"] = terrain_map.shape[1]
-output["terrain_height"] = terrain_map.shape[2]
-output["terrain_map"] = terrain_map.tolist() #convert terrain to JSON serializable format
+output["terrain_steps"] = terrain_maps.shape[0] #convert terrain to JSON serializable format
+output["terrain_width"] = terrain_maps.shape[1]
+output["terrain_height"] = terrain_maps.shape[2]
+output["terrain_map"] = terrain_map = terrain_maps.tolist()[-1] #Only the last terrain map
 
 
 # Generates culture names
-#Culture(1, "The Shire", [250,10,10])
 
-# Saves output file
+
+culture_names = ["The Shire","Mordor","Rohan","The Elves"]
+
+cultureList = []
+
+i = 0
+for name in culture_names:
+    i+=1 # Culture id starts at 1
+    cultureList.append(Culture(i, name, [i,i,i]))
+
+
+output["culture_names"] = culture_names
+
+culture_maps = generate_culture_matrix(terrain_map, cultureList, settings["culturespread_iterationCount"])
+
+culture_map = culture_maps[-1]
+output["culture_map"] = culture_map.tolist()
 
 json_formatted = json.dumps(output)
 javascript_content = "let world_data = "+json_formatted
--- webviewer/script.js
+++ webviewer/script.js
@@ -2,13 +2,11 @@
 
 let settings = {};
 settings.cellSize = 6;
+// settings.culture_color_transparency = 0;
+// settings.culture_color_transparency_hover = 0.9;
 
-// WATER = 1
-// GRASSLAND = 2
-// DESERT = 3
-// JUNGLE = 4
-// MOUNTAIN = 5
-// SNOW = 6
+settings.culture_color_transparency = 0.95;
+settings.culture_color_transparency_hover = 0.95;
 
 let tyle2color =
 [
@@ -22,10 +20,22 @@ let tyle2color =
 ];
 
 
+let culture_colors = shuffle(CSS_COLOR_NAMES);
+
+culture_colors = ["#66c2a5", "#fc8d62", "#8da0cb", "#e78ac3", "#a6d854", "#ffd92f"].concat(culture_colors);
+
+
+
+////////////////////////////////////////////////////////////
 
 let cvs = get("cvs");
 let ctx = cvs.getContext("2d");
-let terrain_map = world_data.terrain_map[world_data.terrain_map.length-1];
+let terrain_map = world_data.terrain_map;
+let culture_map = world_data.culture_map;
+
+let ui = {};
+ui.culture_hover = false;
+ui.culture_click = false;
 
 function for_map(matrix, callback)
 {
@@ -44,22 +54,109 @@ function init()
 
   cvs.width = settings.cellSize*world_data.terrain_width;
   cvs.height = settings.cellSize*world_data.terrain_height;
-  for_map(terrain_map, function(value, x, y){
+  cvs.addEventListener('click', mapClick);
+  cvs.addEventListener('mousemove', mapHover);
 
-    colorSquare(x,y, tyle2color[value])
+  drawMap();
 
-  })
 }
 
 
+function drawMap()
+{
+  ctx.clearRect(0,0,cvs.width, cvs.height);
+  for_map(terrain_map, function(tile_id, x, y){
+
+    colorSquare(x,y, tyle2color[tile_id])
+  })
+
+  for_map(culture_map, function(culture_id, x, y){
+
+    if(culture_id > 0)
+    {
+      if(ui.culture_hover == culture_id)
+      {
+        colorSquare(x,y, "#7FFFD4",settings["culture_color_transparency_hover"])
+      }
+      else
+      {
+        colorSquare(x,y, culture_colors[culture_id],settings["culture_color_transparency"])
+      }
+
+    }
+
+  })
+
+
+  if(ui.culture_hover)
+  {
+    cvs.style.cursor = "pointer";
+  }
+  else
+  {
+    cvs.style.cursor = "default";
+  }
+
+}
+
 function colorSquare(x,y,color,a)
 {
-  if(!a)a=1;
+  if(a == "undefined")a=1;
 
   ctx.globalAlpha = a;
   ctx.fillStyle = color;
   ctx.fillRect(x*settings.cellSize,y*settings.cellSize,settings.cellSize, settings.cellSize);
+  ctx.globalAlpha = 1;
 }
 
 
-init()
+function getCoords(e, boxSize)
+{
+    return{
+      x:Math.floor(e.offsetX / boxSize),
+      y:Math.floor(e.offsetY / boxSize)
+      }
+}
+
+function getCultureFromCoord(coords)
+{
+  if(coords.y < 0 || coords.x < 0) return false;
+
+  if(culture_map[coords.y][coords.x] > 0)
+  {
+    return culture_map[coords.y][coords.x];
+  }
+  else
+  {
+    return false;
+  }
+}
+
+
+function mapClick(e)
+{
+  let coords= getCoords(e, settings.cellSize);
+  ui.culture_click = getCultureFromCoord(coords);
+  drawMap();
+}
+
+function mapHover(e)
+{
+  let coords= getCoords(e, settings.cellSize);
+  ui.culture_hover = getCultureFromCoord(coords);
+  drawMap();
+
+  if(ui.culture_hover!= 0)
+  {
+    get("country_name").innerHTML = world_data["culture_names"][ui.culture_hover-1]
+  }
+  else
+  {
+    get("country_name").innerHTML = "";
+  }
+
+
+}
+
+
+init();
